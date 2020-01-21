d6ab7234ee4057080ff5ecd3134f0478ca7796f8
Red Ochsenbein <red@redochsenbein.ch>
2020-01-21 12:19:46 +0100

    Make sure deleting of a todo can only be done by the user

--- src/routes/todos/deleteTodo.ts
+++ src/routes/todos/deleteTodo.ts
@@ -2,13 +2,15 @@ import { validationResult } from 'express-validator'
 import { getRepository } from 'typeorm'
 
 import { Todo } from '../../entity/Todo'
+import { User } from '../../entity/User'
 
 export const deleteTodo = async (req, res) => {
   const errors = validationResult(req)
   if (!errors.isEmpty()) {
     return res.status(422).json({ success: 0, errors: errors.array() })
   }
+  const user = req.user as User
   const todoRepository = getRepository(Todo)
-  await todoRepository.delete(req.params.id)
+  await todoRepository.delete({ id: req.params.id, user })
   res.json({ success: 1, todo: { id: req.params.id } })
 }
