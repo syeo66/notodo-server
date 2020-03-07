e25e7d86237d475c5ccdd57348b7bef10b3bdf88
Red Ochsenbein <red@redochsenbein.ch>
2020-03-07 16:22:03 +0100

    Add token expiration to login. Refresh token is generated but not yet useful.

--- src/routes/auth/login.ts
+++ src/routes/auth/login.ts
@@ -2,6 +2,7 @@ import { Response, Request } from 'express'
 import { sign } from 'jsonwebtoken'
 import { getRepository } from 'typeorm'
 import * as bcrypt from 'bcrypt'
+import { fromUnixTime } from 'date-fns'
 
 import { User } from '../../entity/User'
 
@@ -17,9 +18,18 @@ export const login = (jwtOptions: { secretOrKey: string }) => async (req: Reques
     if (user && bcrypt.compareSync(password, user.password)) {
       // from now on we'll identify the user by the id and the id is the
       // only personalized value that goes into our token
-      let payload = { id: user.id }
-      let token = sign(payload, jwtOptions.secretOrKey)
-      res.json({ message: 'ok', token: token })
+      const payload = { id: user.id }
+      const tokenExpiresIn = 60 * 60
+      const token = sign(payload, jwtOptions.secretOrKey, { expiresIn: tokenExpiresIn })
+      const refreshTokenExpiresIn = 24 * 60 * 60
+      const refreshToken = sign(payload, jwtOptions.secretOrKey, { expiresIn: refreshTokenExpiresIn })
+      res.json({
+        message: 'ok',
+        token,
+        tokenExpiry: fromUnixTime(Math.floor(Date.now() / 1000) + tokenExpiresIn),
+        refreshToken,
+        refreshTokenExpiry: fromUnixTime(Math.floor(Date.now() / 1000) + refreshTokenExpiresIn),
+      })
     } else {
       res.status(401).json({ message: 'Credentials are incorrect' })
     }
